221268387aa56b5e03171b5d69d421327d07908a
Daniel-Alencar <danielalencar746@gmail.com>
2025-01-30 15:22:45 -0300

    update

--- setup.bat
+++ setup.bat
@@ -3,8 +3,8 @@
 :: Remover o diretório .git
 rmdir /s /q .git
 
-:: Adicionar a linha no CMakeLists.txt
-echo pico_generate_pio_header(pio_matrix ${CMAKE_CURRENT_LIST_DIR}/BDL_lib/led_matrix.pio) >> ../CMakeLists.txt
+:: Adicionar a linha no CMakeLists.txt (assumindo que o arquivo está no diretório pai)
+echo pico_generate_pio_header(pio_matrix ${CMAKE_CURRENT_LIST_DIR}/BDL_lib/led_matrix.pio) >> ..\CMakeLists.txt
 
 echo Comandos executados com sucesso!
 pause
